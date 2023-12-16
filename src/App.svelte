8f690aaca0db537ccfa11c05306009e58f03348e
Nicola Toscan <nicolatoscan99@outlook.it>
2023-12-17 00:59:28 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Header from "./components/Top.svelte";
+</script>
+
+<div>
+  <div id="header" class="header"><Header /></div>
+</div>
+
+<style>
+  :global(*) {
+    box-sizing: border-box;
+  }
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    font-family: monospace;
+  }
+  .header {
+    margin-bottom: 100vh;
+  }
+</style>
---
+++ src/components/Top.svelte
@@ -0,0 +1,42 @@
+<div class="header">
+    <div class="title">
+        <h1> <span class="black">Car<span class="orange">Hub</span></span></h1>
+        <h2>Let's accelerate the development of autonomous vehicles</h2>
+    </div>
+</div>
+
+<style lang="less">
+    .header {
+        background-image: url(../images/header.jpeg);
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        height: 100vh;
+        width: 100vw;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-attachment: fixed;
+
+        .title {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            margin-top: 10vh;
+        }
+
+
+        h1, h2 {
+            margin: 0;
+            text-align: center;
+            padding: 0 2em;
+            color: #444;
+            display: inline-block;
+        }
+
+        h1 {
+            margin-bottom: 1rem;
+            font-size: 6rem;
+        }
+    }
+</style>
